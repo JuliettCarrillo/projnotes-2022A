6b166bfb05f1a896980c0aba396fcfafbdeab75c
JuliettCarrillo <l181130077@gamadero.tecnm.mx>
2022-03-30 07:55:48 -0600

    Testing de ES06 en Front-end

--- client/index.js
+++ client/index.js
@@ -1,2 +1,26 @@
-console.log("Webpack Working!!!!");
- 
+const async = require("hbs/lib/async");
+
+console.log("😎Webpack Working👻!!!!");
+ //Default parameters
+
+ //arouwn funtions
+ let show =(m="❌")=>{
+    console.log(m);
+ };
+ show();
+//promises
+function resolveAfter2Seconds(){
+    return new Promise((resolve) =>{
+        setTimeout(()=>{
+            resolve("function resolve")
+        },2000);
+    });
+}
+//funcion asincrona
+async function asyncCall(){
+    console.log("Calling asyn function!!");
+    const result=await resolveAfter2Seconds();
+    console.log(result);// Imprime function resolve en la consola
+}
+asyncCall();
+     
